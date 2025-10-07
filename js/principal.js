1d82aef68ecbc69bebc5505192d8337e6368ade1
ygor <ygor.marquezini@escola.pr.gov.br>
2025-10-07 12:04:37 +0000

    s

---
+++ js/principal.js
@@ -0,0 +1,15 @@
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent=("plk nutricao");
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso")
+var peso = tdPeso.textContent;
+console.log(paciente);
+console.log(peso);
+
+var tdAltura = document.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+console.log(altura);
