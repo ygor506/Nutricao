1bd2d62b6baaeca67ece40d1c2faae74d4d102ef
ygor <ygor.marquezini@escola.pr.gov.br>
2025-10-07 12:48:03 +0000

    gbb

--- js/principal.js
+++ js/principal.js
@@ -5,12 +5,18 @@ titulo.textContent=("igor nutricao");
 
 
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(nome);
+vartdnome = document.querySelector(" .info-nome")
+
 
 var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(peso);
 
+//07/10/2025
 var tdAltura = document.querySelector(".info-altura")
 var altura = tdAltura.textContent;
 console.log(altura);
+
+
+var imc = peso/ altura * altura;
+console.log(imc)
