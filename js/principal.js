8bc4582417e62c9986bed0a5e810812fcb7de087
ygor <ygor.marquezini@escola.pr.gov.br>
2025-10-31 14:05:29 +0000

    6666

--- js/principal.js
+++ js/principal.js
@@ -1,10 +1,59 @@
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent=("pinpin nutricao");
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i=0; i <pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+// 10/10
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+
+if (peso <=0||peso>=1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent="Peso inválido!";
+    paciente.classList.add("paciente-invalido")
+}
+
+if (altura <=0||altura>=3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent= "Altura inválida!";
+    paciente.classList.add("paciente-invalido");
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+}
+}
+
+titulo.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+    alert("Olá eu fui clicado.")
+}
+
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("cliclk", function(event){
 event.preventDefault();
 alert("Oi eu sou o botao e fui clicado");
-
-
-var from = document.querySelector("#form-adiciona");
+var form = document.querySelector("#form-adiciona");
 
 
 var nome = from.nome.value;
