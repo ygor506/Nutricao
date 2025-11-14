a64c204583359cc16524d45be00f71dc1637f277
ygor <ygor.marquezini@escola.pr.gov.br>
2025-11-14 11:58:07 +0000

    44

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,45 +1,38 @@
 var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent = ("Nego nutritations");
+titulo.textContent = ("Vlá Nutrição");
 
 var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) { // abre chaves do for 
+    var paciente = pacientes[i];
 
-for(var i = 0; i < pacientes.length; i++) {
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var paciente = pacientes[i];
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-var tdImc = paciente.querySelector(".info-imc");
+    if (peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-var pesoEhValido = true;
-var alturaEhValido = true;                                      
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if (peso <= 0 || peso >= 1000){
-console.log("Peso inválido!");
-pesoEhvalido = false;
-tdImc.textContent = "Peso inválido!";
-paciente.classList.add("paciente-invalido");
-}
-
-if (altura <= 0 || altura >= 3.00){
-console.log("Altura inválida!");
-alturaEhValido = false;
-tdImc.textContent = "Altura inválida!";
-paciente.classList.add("paciente-invalido");
-}
-
-if (alturaEhValido && pesoEhValido){
-var imc = calculaImc(peso,altura);
-tdImc.textContent = imc.toFixed(2);
-  }
-}
-
+  if (alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+} 
 
 function calculaImc (peso, altura){
     var imc = 0;
